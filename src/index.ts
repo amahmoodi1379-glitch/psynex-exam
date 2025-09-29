5adf505b2e1cbf3aaaa2b0276eea4c3ff96533db
amahmoodi1379-glitch <a.mahmoodi1379@gmail.com>
2025-09-29 17:06:47 +0330

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
   fetch(_request: Request): Response {
-    return new Response("OK - from GitHub repo skeleton", {
+    return new Response("OK - from GitHub deploy 1", {
       headers: { "content-type": "text/plain; charset=utf-8" }
     });
   }
